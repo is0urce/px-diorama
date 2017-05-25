e5ccdeafc888764933777c72fdad392d39139825
is0urce <azathoth@mail.ru>
2017-05-25 19:44:36 +0300

    sk_i_pain

--- px-diorama/data/scripts/sk_i_pain.lua
+++ px-diorama/data/scripts/sk_i_pain.lua
@@ -3,19 +3,17 @@
 -- auth: is0urce
 -- desc: skill script
 
-tag = "sk_s_smite"
+tag = "sk_i_pain"
 
 -- state attributes
 
 duration = 1
 hostile = true
 targeted = true
-min_range = 0
-max_range = 1
 
-name = "Smite Weak"
-alias = "Smite"
-description = "Do 200% weapon damage"
+name = "World of Pain: Pain"
+alias = "Pain"
+description = "Do 500% damage over 20 rounds"
 
 -- returns true if skill can be used
 
@@ -25,18 +23,17 @@ condition = function(user, target)
 		and not user:dead()
 		and not target:dead()
 --		and game:reputation(user, target) < 0 -- friend-or-foe check, negative is hostile
-		and min_range <= distance and distance <= max_range -- in range
 end
 
 -- action taken if skill is used
 
 action = function(user, target)
 	local weapon_damage, variant, hit, critical = game:hit(user, target)
-	local result_damage = math.floor(weapon_damage * 2.0)
+	local result_damage = math.floor(weapon_damage * 5.0)
 
 	if hit then
 		target:damage(1)
 		game:popup(target:position(), result_damage, 0xffcc00, 1.0)
-		game:vfx(target:position(), "e_swipe")
+		game:vfx_projectile(user:position(), target:position(), "@")
 	end
 end
